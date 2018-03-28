018c08d85f5c992b24af039b24a44cc22156ad2a
Dan Conley <dconley@protonmail.com>
2018-03-28 13:40:30 -0400

    refactoring to use cobra

---
+++ cmd/album.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/Sigafoos/lastfm"
+	"github.com/spf13/cobra"
+)
+
+var (
+	requireMBID bool
+	mbid        string
+)
+
+func init() {
+	rootCmd.AddCommand(albumScrobble)
+
+	rootCmd.Flags().BoolVarP(&requireMBID, "requireMBID", "r", true, "Require results to have a MusicBrainz ID")
+	rootCmd.Flags().StringVarP(&mbid, "mbid", "m", "", "A MusicBrainz ID to use instead of searching (note that not all mbids are in Last.FM)")
+}
+
+var albumScrobble = &cobra.Command{
+	Use:   "album",
+	Short: "Scrobble all tracks on an album",
+	Long: `Search for an album, either by title or MusicBrainz ID.
+
+Then retrieve the tracks from Last.FM and scrobble them all in a bulk command.`,
+	Run: scrobbleAlbum,
+}
+
+func scrobbleAlbum(cmd *cobra.Command, args []string) {
+	lfm.SetVerbose(verbose)
+
+	var album lastfm.Album
+
+	if mbid != "" {
+		album = lastfm.Album{MusicBrainzID: mbid}
+	} else {
+		fmt.Print("album title: ")
+		title, _ := cli.ReadString('\n')
+		title = strings.TrimSpace(title)
+
+		albums, err := lfm.AlbumSearch(title, requireMBID)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if len(albums) == 0 {
+			fmt.Printf("\nNo results found for \"%s\"\n", title)
+			os.Exit(0)
+		}
+
+		fmt.Println()
+		for i := 0; i < 5 && i < len(albums); i++ {
+			album := albums[i]
+			fmt.Printf("[%v] \"%s\" - %s\n", i+1, album.Name, album.Artist)
+		}
+
+		fmt.Print("\nChoose a result (0 to exit): ")
+		text, _ := cli.ReadString('\n')
+		text = strings.TrimSpace(text)
+		i, err := strconv.Atoi(text)
+
+		if err != nil || i < 0 || i > 5 || i > len(albums) {
+			fmt.Println("invalid entry")
+			os.Exit(1)
+		} else if i == 0 {
+			os.Exit(0)
+		}
+
+		album = albums[i-1]
+	}
+
+	if verbose {
+		fmt.Printf("MBID: %s\n", album.MusicBrainzID)
+	}
+
+	album, err := lfm.AlbumInfo(album)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Scrobbling %v tracks from %s - \"%s\"\n\n", len(album.TrackList.Tracks), album.Artist, album.Name)
+	err = lfm.ScrobbleAlbum(album)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/root.go
@@ -0,0 +1,94 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/Sigafoos/lastfm"
+
+	"github.com/spf13/cobra"
+)
+
+// APIKey and APISecret are the key and shared secret for a last.fm app. If you're building this from source
+// you'll need your own, and to build it with a compile time linker flag:
+//
+//   go build -ldflags "-X github.com/Sigafoos/scrobble/cmd.APIKey=<your key> -X github.com/Sigafoos/scrobble/cmd.APISecret=<your secret>" -o scrobble github.com/Sigafoos/scrobble/cmd.go
+var (
+	APIKey    string
+	APISecret string
+)
+
+var (
+	lfm     *lastfm.LastFM
+	cli     *bufio.Reader
+	verbose bool
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "scrobble",
+	Short: "Scrobble tracks and albums to Last.FM",
+	Long: `something coming soon
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(cmd.UsageString())
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	if APIKey == "" || APISecret == "" {
+		fmt.Println("fatal error: API key/secret not found. was there a compile-time error?")
+		os.Exit(1)
+	}
+
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+
+	cli = bufio.NewReader(os.Stdin)
+	lfm = lastfm.New(APIKey, APISecret, false) // verbose is set after the flags have been parsed
+	lfm.SetSessionKey(getSessionToken())
+}
+
+func getSessionToken() string {
+	usr, _ := user.Current()
+	filename := fmt.Sprintf("%s/.lastfm", usr.HomeDir)
+	b, err := ioutil.ReadFile(filename)
+
+	if err == nil {
+		return string(b)
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	fmt.Println("Generating session token (your password will NOT be stored)...\n")
+	fmt.Print("last.fm username: ")
+	username, _ := cli.ReadString('\n')
+	fmt.Print("last.fm password: ")
+	password, _ := cli.ReadString('\n')
+
+	token, err := lfm.Authenticate(strings.TrimSpace(username), strings.TrimSpace(password))
+	if err != nil {
+		fmt.Println(err)
+		f.Close() // won't trigger defer on os.Exit()
+		os.Remove(filename)
+		os.Exit(1)
+	}
+	f.WriteString(token)
+	f.Sync()
+
+	return token
+}
